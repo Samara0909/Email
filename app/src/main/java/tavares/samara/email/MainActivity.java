bad6443dfb0c5e1e4b95dda62cdb5abe9bd7dd51
20221TIIMI0262 <samaratavaress0906@gmail.com>
2024-04-05 11:08:40 -0300

    inicio

--- app/src/main/java/tavares/samara/email/MainActivity.java
+++ app/src/main/java/tavares/samara/email/MainActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnEnviar = (Button) findViewById(R.id.btnEnviar); // Definicao da acao do click do botao
+      btnEnviar.setOnClickListener(new View.OnClickListener() {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+            public void onClick(View v) {
+
+            });
+
+        }
 }
